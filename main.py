a7d917401b6dd889d876bd0bf8a1e4ded98b8c74
Svitlana Shulha <svitlash08@gmail.com>
2023-12-17 19:35:11 +0200

    main_classes_structure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import re
 from datetime import datetime
 from collections import UserDict
-from _collections_abc import Iterator
 
 class Field:                                        #parrent 
     def __init__(self, value=None):
@@ -55,15 +54,10 @@ class Email(Field):
         else:
             raise ValueError("Email is not valid")
 
-class Note(Field):
-    @Field.value.setter
-    def value(self, value):
-        self._Field__value = value
-
-class Address(Field):
-    @Field.value.setter
-    def value(self, value):
-        self._Field__value = value
+class Note:
+    pass
+class Address:
+    pass
 
 class Record: 
     def __init__(self, name, phone, birthday, email, address=None, note=None) -> None:
@@ -74,8 +68,7 @@ class Record:
         self.email = Email(email)
         self.address = Address(address)
         self.note = Note(note)         
-        self.notes = [self.note] if note else []
-        #from __str__:  
+        self.notes = [self.note] if note else []  
 
     def add_phone(self, phone_number:str): # adding phone
         phone = Phone(phone_number)  
@@ -96,7 +89,6 @@ class Record:
             if phone.value == old_phone:
                 new_phone_obj = Phone(new_phone) 
                 self.phones[i] = new_phone_obj   
-
 
     def find_phone(self, phone_number:str): # find phone if it exist 
         phone = Phone(phone_number)
@@ -130,50 +122,4 @@ class Record:
     
     def __str__(self) -> str: # readable view
         return f"contact name:{self.name.value}, phones:{'; '.join(i.value for i in self.phones)}, birthday:{self.birthday.value}, email:{self.email.value}, address:{self.address.value if self.address.value is not None else 'N/A'}, note:{'; '.join(i.value for i in self.notes) if self.note.value is not None else 'N/A'}"
-
-class AddressBook(UserDict):
-    def add_record(self, record: Record): # add record in dictionary
-        key = record.name.value
-        self.data[key] = record
-
-    def find_record(self, name):   # get record from dictionary
-       return self.data.get(name)
-
-    def delete_record(self, name):  # delete record in dictionary
-        pass
-
-    def __iter__(self) -> Iterator:
-        return AddressBookIterator(self.data.values(), page_size=5)
-   
-    def __repr__(self):
-        return f"AddressBook({self.data})"
-  
-class AddressBookIterator:
-    def __init__(self, records_list, page_size):
-        self.records = list(records_list)
-        self.page_size = page_size
-        self.counter = 0  # quantity on page
-        self.page = len(self.records) // self.page_size # use for showing part of the reccords that size < page_size
     
-    def __next__(self):
-        if self.counter >= len(self.records):
-            raise StopIteration
-        else:
-            if self.page > 0:
-                result = list(self.records[self.counter:self.counter + self.page_size]) # slice reccords on the page
-                self.page -= 1
-                self.counter += self.page_size
-            else:
-                result = list(self.records[self.counter:])  #the rest of the records on the page
-                self.counter = len(self.records)
-        return result
-
-record1 = Record("John Doe", "0989990008", "1985-12-15", "ghgjg9898@mail.ua", "some_address", 'unknown person')
-record1.add_phone("0934567677")
-record1.add_phone("0934888885")
-record1.add_note('my collegue')
-print(record1)
-adr_book = AddressBook()
-# adr_book.add_record(record1)
-# print(adr_book.find('John Doe'))
-
